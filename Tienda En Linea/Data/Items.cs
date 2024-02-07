f16d2c3c599fb385e36696f5bb246a6c7ab32f9e
JeanP99 <156860098+JeanP99@users.noreply.github.com>
2024-02-06 20:37:23 -0400

    Agregado area en la pagina para Tienda y una clase llamada Items

---
+++ Tienda En Linea/Data/Items.cs
@@ -0,0 +1,10 @@
+﻿namespace Tienda_En_Linea.Data
+{
+    public class Items
+    {
+        public string Nombre { get; set; }    
+        public string Descripcion { get; set; } 
+        public string Tipo { get; set; }    
+        public int Precio { get; set; }    
+    }
+}
